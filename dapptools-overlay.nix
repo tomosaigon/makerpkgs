26dd5b24a587041b24a3a1e8a56fb86547739ba2
tomo <tomo@crowdeo.com>
2025-12-01 17:51:50 +0700

    compat: fix dapptools tarball URL (single .tar.gz)

--- dapptools-overlay.nix
+++ dapptools-overlay.nix
@@ -11,7 +11,7 @@ rec {
 
   master-20220406 = fetchTarball {
     name = "dapptools-master-20220406";
-    url = "https://github.com/tomosaigon/dapptools/archive/4edcc2f77d32630dc65b4c1c255e50932d121dcc.tar.gz.tar.gz";
+    url = "https://github.com/tomosaigon/dapptools/archive/4edcc2f77d32630dc65b4c1c255e50932d121dcc.tar.gz";
     sha256 = "1a387bpcw0n3g3649hc4sari9zwwm6bf4spcdlsinklpzxk7277w";
   };
 
