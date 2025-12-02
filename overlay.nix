4e44f84f67a90f69f3b76a4b2cacc05e6756b4a9
tomo <tomo@crowdeo.com>
2025-12-02 10:38:01 +0700

    compat: redefine semver-range to use Hackage tarball

--- overlay.nix
+++ overlay.nix
@@ -71,18 +71,51 @@ let
 
     makerScriptPackage = self.callPackage ./script-builder.nix {};
   };
+
 in (makerpkgs { inherit dapptoolsOverrides; }) // {
   makerpkgs = makeOverridable makerpkgs { inherit dapptoolsOverrides; };
 
-  # Fix semver-range-0.2.8 globally for the default Haskell package set.
+  # Fix semver-range-0.2.8 globally for the default Haskell package set
+  # by redefining it from scratch (no dependency on the old patch-1 GitHub tag).
   haskellPackages = super.haskellPackages.override {
     overrides = self: super: {
-      semver-range = super.semver-range.overrideAttrs (attrs: {
-        src = super.fetchurl {
-          url = "https://hackage.haskell.org/package/semver-range-0.2.8/semver-range-0.2.8.tar.gz";
-          sha256 = "1df663zkcf7y7a8cf5llf111rx4bsflhsi3fr1f840y4kdgxlvkf";
-        };
-      });
+      semver-range =
+        super.callPackage
+          ({ mkDerivation
+           , base
+           , classy-prelude
+           , fetchurl
+           , hspec
+           , parsec
+           , QuickCheck
+           , text
+           , unordered-containers
+           }:
+           mkDerivation {
+             pname = "semver-range";
+             version = "0.2.8";
+
+             src = fetchurl {
+               url    = "https://hackage.haskell.org/package/semver-range-0.2.8/semver-range-0.2.8.tar.gz";
+               sha256 = "1df663zkcf7y7a8cf5llf111rx4bsflhsi3fr1f840y4kdgxlvkf";
+             };
+
+             libraryHaskellDepends = [
+               base
+               classy-prelude
+               parsec
+               text
+               unordered-containers
+             ];
+             testHaskellDepends = [
+               base
+               hspec
+               QuickCheck
+             ];
+
+             description =
+               "Representation, manipulation, and de/serialisation of Semantic Versions";
+           }) {};
     };
   };
 }
