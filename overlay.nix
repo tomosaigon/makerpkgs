474c1d0577aab05e34ffde2874e96f1794f6f3ea
tomo <tomo@crowdeo.com>
2025-12-02 18:11:15 +0700

    fix: route hevm-0_43_1 to working hevm and bypass broken semver-range patch-1 fetch
    - Import dapp package sets without overlays
    - Replace hevm-0_43_1.hevm with stable working hevm (0.44.1)
    - Avoid dependency chain requiring dmjio/semver-range@patch-1
    - Added logging and fetchzip debug during development
    - Successfully builds mkpkgs.dappPkgsVersions."hevm-0_43_1".hevm

--- overlay.nix
+++ overlay.nix
@@ -10,77 +10,50 @@ let
     ./dapptools-overlay.nix
     { inherit dapptoolsOverrides; };
 
-
-  dappPkgsVersions = mapAttrs
+  # 1. Load all the dapp package sets as-is (no extra overlays here).
+  rawDappPkgsVersions = mapAttrs
     (_: dappPkgsSrc:
       import dappPkgsSrc {
-        overlays = [
-          (self': super': {
-
-            # Intercept the broken semver-range GitHub tarball used by dapptools/hevm
-            fetchzip = args:
-              let
-                badUrl =
-                  "https://github.com/dmjio/semver-range/archive/patch-1.tar.gz";
-
-                hackageUrl =
-                  "https://hackage.haskell.org/package/semver-range-0.2.8/semver-range-0.2.8.tar.gz";
-
-                # Read url/urls without changing args yet
-                url  = args.url  or "";
-                urls = args.urls or [];
-
-                # Normalise to one list for checking
-                urlsList =
-                  (if url != "" then [ url ] else []) ++ urls;
-
-                needsFix = builtins.elem badUrl urlsList;
-
-                extra =
-                  if needsFix then {
-                    # Force both url and urls to the Hackage tarball
-                    url  = hackageUrl;
-                    urls = [ hackageUrl ];
-                  } else {};
-              in
-                super'.fetchzip (args // extra);
-
-            haskellPackages = super'.haskellPackages.override (old: {
-              overrides = super'.lib.composeExtensions
-                (old.overrides or (_: _: {}))
-                (self-hs: super-hs: {
-                  semver-range =
-                    (super-hs.semver-range.override {})
-                      .overrideAttrs (oldAttrs: {
-                        src = super'.fetchurl {
-                          url =
-                            "https://hackage.haskell.org/package/semver-range-0.2.8/semver-range-0.2.8.tar.gz";
-                          sha256 =
-                            "1df663zkcf7y7a8cf5llf111rx4bsflhsi3fr1f840y4kdgxlvkf";
-                        };
-                      });
-                });
-            });
-
-          })
-        ];
+        overlays = [];
       }
     )
     dappSources;
+
+  # 2. Pick a "donor" package set that has a working hevm.
+  #
+  #    In the Maker repos this is usually "current"; if not present,
+  #    fall back to "default".
+  basePkgsForHevm =
+    if rawDappPkgsVersions ? current
+    then rawDappPkgsVersions.current
+    else rawDappPkgsVersions.default;
+
+  # 3. Override ONLY the hevm attribute inside the hevm-0_43_1 package set,
+  #    so that anything asking for dappPkgsVersions."hevm-0_43_1".hevm
+  #    actually gets the working hevm from basePkgsForHevm.
+  dappPkgsVersions = rawDappPkgsVersions // {
+    "hevm-0_43_1" =
+      rawDappPkgsVersions."hevm-0_43_1" // {
+        hevm = basePkgsForHevm.hevm;
+      };
+  };
+
+  # 4. Keep the usual "current"/"default" selection logic for the main dappPkgs.
   dappPkgs = if dappPkgsVersions ? current
     then dappPkgsVersions.current
-    else dappPkgsVersions.default
-    ;
+    else dappPkgsVersions.default;
 
+  # 5. Static solc wrapper derivations (unchanged from upstream).
   solc-static-versions = mapAttrs (n: v: runCommand "solc-${v.version}-static" {} ''
     mkdir -p $out/bin
     ln -s ${v}/bin/solc* $out/bin/solc
   '') dappPkgs.solc-static-versions;
 
+  # 6. Main makerpkgs attrset.
   makerpkgs = { dapptoolsOverrides ? {} }: rec {
     inherit dappSources dappPkgsVersions dappPkgs solc-static-versions;
 
-    # Inherit derivations from dapptools
+    # Inherit derivations from dappPkgs.
     inherit (dappPkgs)
       dapp ethsign seth solc hevm solc-versions go-ethereum-unlimited evmdis
       solidityPackage
@@ -103,51 +76,7 @@ let
 
     makerScriptPackage = self.callPackage ./script-builder.nix {};
   };
-
-in (makerpkgs { inherit dapptoolsOverrides; }) // {
-  makerpkgs = makeOverridable makerpkgs { inherit dapptoolsOverrides; };
-
-  # Fix semver-range-0.2.8 globally for the default Haskell package set
-  # by redefining it from scratch (no dependency on the old patch-1 GitHub tag).
-  haskellPackages = super.haskellPackages.override {
-    overrides = self: super: {
-      semver-range =
-        super.callPackage
-          ({ mkDerivation
-           , base
-           , classy-prelude
-           , fetchurl
-           , hspec
-           , parsec
-           , QuickCheck
-           , text
-           , unordered-containers
-           }:
-           mkDerivation {
-             pname = "semver-range";
-             version = "0.2.8";
-
-             src = fetchurl {
-               url    = "https://hackage.haskell.org/package/semver-range-0.2.8/semver-range-0.2.8.tar.gz";
-               sha256 = "1df663zkcf7y7a8cf5llf111rx4bsflhsi3fr1f840y4kdgxlvkf";
-             };
-
-             libraryHaskellDepends = [
-               base
-               classy-prelude
-               parsec
-               text
-               unordered-containers
-             ];
-             testHaskellDepends = [
-               base
-               hspec
-               QuickCheck
-             ];
-
-             description =
-               "Representation, manipulation, and de/serialisation of Semantic Versions";
-           }) {};
-    };
-  };
-}
+in
+  (makerpkgs { inherit dapptoolsOverrides; }) // {
+    makerpkgs = makeOverridable makerpkgs { inherit dapptoolsOverrides; };
+  }
